7df4f5622cd6204774c0aa27b80dd1aa7ef1745f
NgHuyen <nguyenngocnhuhuyen1307@gmail.com>
2025-06-09 03:23:19 +0700

    Update code for FE deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || "/AutoTask",
-
-  resolve: {
-    alias: [
-      { find: '~', replacement: '/src' }
-    ]
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    base: import.meta.env.VITE_BASE_PATH || "/AutoTask",
+  
+    resolve: {
+      alias: [
+        { find: '~', replacement: '/src' }
+      ]
+    }
   }
 })
