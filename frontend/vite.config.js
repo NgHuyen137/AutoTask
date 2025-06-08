7fdb89db4d145ab301a19cef8bbacfdb4ead3327
NgHuyen <nguyenngocnhuhuyen1307@gmail.com>
2025-06-09 04:30:13 +0700

    Remove VITE_BASE_PATH

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [react()],
-    base: import.meta.env.VITE_BASE_PATH || "/AutoTask",
-  
+    plugins: [react()],  
     resolve: {
       alias: [
         { find: '~', replacement: '/src' }
