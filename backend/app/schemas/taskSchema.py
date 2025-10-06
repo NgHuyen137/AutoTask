965928de9cbb137ccc240dfe2422a602fcdc16bf
NgHuyen <nguyenngocnhuhuyen1307@gmail.com>
2025-10-07 03:00:55 +0700

    feat: add sort, search, filter, pagination for tasks

--- backend/app/schemas/taskSchema.py
+++ backend/app/schemas/taskSchema.py
@@ -1,111 +1,108 @@
-from datetime import datetime
-from enum import Enum, IntEnum
+from datetime import date, datetime
 from typing import Any, List, Optional
 
 from beanie import PydanticObjectId
 from pydantic import BaseModel, Field, field_validator, model_validator
 
+from app.types.taskTypes import Priority, Status, RangeType, SortOrder, SortBy
 from app.utils.datetime import add_utc_timezone, get_utc_now
 
 
-class Status(IntEnum):
-	NOT_COMPLETED = 0
-	COMPLETED = 1
-
-
-class Priority(IntEnum):
-	ASAP = 3
-	HIGH = 2
-	MEDIUM = 1
-	LOW = 0
-
-
-class TimeUnit(str, Enum):
-	MINUTES = "minutes"
-	HOURS = "hours"
-	DAYS = "days"
-	WEEKS = "weeks"
-
-
 class Tag(BaseModel):
-	name: str
-	is_preferred: bool = False
+  name: str
+  is_preferred: bool = False
 
 
 class TimeBlock(BaseModel):
-	start_at: datetime
-	end_at: datetime
-	is_scheduled_ontime: bool = True
+  start_at: datetime
+  end_at: datetime
+  is_scheduled_ontime: bool = True
 
-	@field_validator("start_at", "end_at", mode="after")
-	@classmethod
-	def add_timezone(cls, value: datetime):
-		if not value.tzinfo:
-			return add_utc_timezone(value)
-		return value
+  @field_validator("start_at", "end_at", mode="after")
+  @classmethod
+  def add_timezone(cls, value: datetime):
+    if not value.tzinfo:
+      return add_utc_timezone(value)
+    return value
 
 
 class Duration(BaseModel):
-	hours: int
-	minutes: int
+  hours: int
+  minutes: int
 
 
 class Split(BaseModel):
-	min_duration: Duration
+  min_duration: Duration
+
+
+class TaskFilter(BaseModel):
+  start_of_week: Optional[str] = None
+  end_of_week: Optional[str] = None
+  search: Optional[str] = None
+  status: Optional[Status] = None
+  priority: Optional[Priority] = None
+  tags: Optional[str] = None
+  range_type: Optional[RangeType] = None
+  from_date: Optional[date] = None
+  to_date: Optional[date] = None
+  sort_by: SortBy = "due_date"
+  sort_order: SortOrder = "asc"
+  page: int = Field(default=1, ge=1)
+  limit: int = Field(default=10, ge=1, le=100)
 
 
 class TaskCreate(BaseModel):
-	scheduling_hour_id: Optional[PydanticObjectId] = None
-	smart_scheduling: bool = False
-	name: str
-	status: Status = Status.NOT_COMPLETED
-	priority: Priority = Priority.MEDIUM
-	tags: Optional[List[Tag]] = None
-	description: Optional[str] = None
-	duration: Optional[Duration] = None
-	split: Optional[Split] = None
-	start_date: Optional[datetime] = None
-	due_date: Optional[datetime] = None
-	time_allocations: Optional[List[TimeBlock]] = None
-	created_at: datetime = Field(default_factory=get_utc_now)
+  scheduling_hour_id: Optional[PydanticObjectId] = None
+  smart_scheduling: bool = False
+  name: str
+  status: Status = Status.NOT_COMPLETED
+  priority: Priority = Priority.MEDIUM
+  tags: Optional[List[Tag]] = None
+  description: Optional[str] = None
+  duration: Optional[Duration] = None
+  split: Optional[Split] = None
+  start_date: Optional[datetime] = None
+  due_date: Optional[datetime] = None
+  time_allocations: Optional[List[TimeBlock]] = None
+  created_at: datetime = Field(default_factory=get_utc_now)
 
-	@model_validator(mode="after")
-	@classmethod
-	def add_timezone(cls, data: Any):
-		if data.start_date and not data.start_date.tzinfo:
-			data.start_date = add_utc_timezone(data.start_date)
-		if data.due_date and not data.due_date.tzinfo:
-			data.due_date = add_utc_timezone(data.due_date)
-		if data.created_at and not data.created_at.tzinfo:
-			data.created_at = add_utc_timezone(data.created_at)
-		return data
+  @model_validator(mode="after")
+  @classmethod
+  def add_timezone(cls, data: Any):
+    if data.start_date and not data.start_date.tzinfo:
+      data.start_date = add_utc_timezone(data.start_date)
+    if data.due_date and not data.due_date.tzinfo:
+      data.due_date = add_utc_timezone(data.due_date)
+    if data.created_at and not data.created_at.tzinfo:
+      data.created_at = add_utc_timezone(data.created_at)
+    return data
 
 
 class TaskUpdate(BaseModel):
-	scheduling_hour_id: Optional[PydanticObjectId] = None
-	smart_scheduling: bool = False
-	name: str
-	status: Status
-	priority: Priority
-	tags: Optional[List[Tag]] = None
-	description: Optional[str] = None
-	duration: Optional[Duration] = None
-	split: Optional[Split] = None
-	start_date: Optional[datetime] = None
-	due_date: Optional[datetime] = None
-	time_allocations: Optional[List[TimeBlock]] = None
-	created_at: datetime
-	updated_at: datetime = Field(default_factory=get_utc_now)
+  scheduling_hour_id: Optional[PydanticObjectId] = None
+  smart_scheduling: bool = False
+  name: str
+  status: Status
+  priority: Priority
+  tags: Optional[List[Tag]] = None
+  description: Optional[str] = None
+  duration: Optional[Duration] = None
+  split: Optional[Split] = None
+  start_date: Optional[datetime] = None
+  due_date: Optional[datetime] = None
+  time_allocations: Optional[List[TimeBlock]] = None
+  created_at: datetime
+  updated_at: datetime = Field(default_factory=get_utc_now)
 
-	@model_validator(mode="after")
-	@classmethod
-	def add_timezone(cls, data: Any):
-		if data.start_date and not data.start_date.tzinfo:
-			data.start_date = add_utc_timezone(data.start_date)
-		if data.due_date and not data.due_date.tzinfo:
-			data.due_date = add_utc_timezone(data.due_date)
-		if data.created_at and not data.created_at.tzinfo:
-			data.created_at = add_utc_timezone(data.created_at)
-		if data.updated_at and not data.updated_at.tzinfo:
-			data.updated_at = add_utc_timezone(data.updated_at)
-		return data
+  @model_validator(mode="after")
+  @classmethod
+  def add_timezone(cls, data: Any):
+    if data.start_date and not data.start_date.tzinfo:
+      data.start_date = add_utc_timezone(data.start_date)
+    if data.due_date and not data.due_date.tzinfo:
+      data.due_date = add_utc_timezone(data.due_date)
+    if data.created_at and not data.created_at.tzinfo:
+      data.created_at = add_utc_timezone(data.created_at)
+    if data.updated_at and not data.updated_at.tzinfo:
+      data.updated_at = add_utc_timezone(data.updated_at)
+    return data
--- backend/app/services/taskService/crud.py
+++ backend/app/services/taskService/crud.py
@@ -1,144 +1,222 @@
+from datetime import time, datetime
 from beanie import PydanticObjectId
 
 from app.models.taskModel import Task
-from app.schemas.taskSchema import TaskCreate, TaskUpdate
+from app.schemas.taskSchema import TaskCreate, TaskUpdate, TaskFilter
 
 
 async def get_task_by_id(id: str):
-	task = await Task.get(id)
-	if task:
-		return task
-	return None
+  task = await Task.get(id)
+  if task:
+    return task
+  return None
 
 
-async def get_tasks(start_of_week: str, end_of_week: str):
-	results = await Task.aggregate(
-		[
-			{"$unwind": "$time_allocations"},
-			{
-				"$addFields": {
-					"task_start_at": {
-						"$dateToString": {
-							"format": "%Y-%m-%d",
-							"date": "$time_allocations.start_at",
-						}
-					}
-				}
-			},
-			{"$match": {"task_start_at": {"$gte": start_of_week, "$lte": end_of_week}}},
-			{
-				"$group": {
-					"_id": "$_id",
-					"scheduling_hour_id": {"$first": "$scheduling_hour_id"},
-					"smart_scheduling": {"$first": "$smart_scheduling"},
-					"name": {"$first": "$name"},
-					"priority": {"$first": "$priority"},
-					"status": {"$first": "$status"},
-					"tags": {"$first": "$tags"},
-					"duration": {"$first": "$duration"},
-					"split": {"$first": "$split"},
-					"start_date": {"$first": "$start_date"},
-					"due_date": {"$first": "$due_date"},
-					"created_at": {"$first": "$created_at"},
-					"updated_at": {"$first": "$updated_at"},
-					"description": {"$first": "$description"},
-					"time_allocations": {"$push": "$time_allocations"},
-				}
-			},
-			{"$project": {"task_start_at": 0}},
-		]
-	).to_list()
+async def get_tasks(filters: TaskFilter):
+  pipeline = []
 
-	if results:
-		tasks = []
-		for task in results:
-			task["id"] = task.pop("_id")
-			tasks.append(Task(**task))
-		return tasks
-	return None
+  if filters.search:
+    pipeline.append({"$match": {"$text": {"$search": filters.search}}})
+
+  if filters.start_of_week and filters.end_of_week:
+    pipeline = [
+      {"$unwind": "$time_allocations"},
+      {
+        "$addFields": {
+          "task_start_at": {
+            "$dateToString": {
+              "format": "%Y-%m-%d",
+              "date": "$time_allocations.start_at",
+            }
+          }
+        }
+      },
+      {
+        "$match": {
+          "task_start_at": {"$gte": filters.start_of_week, "$lte": filters.end_of_week}
+        }
+      },
+      {
+        "$group": {
+          "_id": "$_id",
+          "scheduling_hour_id": {"$first": "$scheduling_hour_id"},
+          "smart_scheduling": {"$first": "$smart_scheduling"},
+          "name": {"$first": "$name"},
+          "priority": {"$first": "$priority"},
+          "status": {"$first": "$status"},
+          "tags": {"$first": "$tags"},
+          "duration": {"$first": "$duration"},
+          "split": {"$first": "$split"},
+          "start_date": {"$first": "$start_date"},
+          "due_date": {"$first": "$due_date"},
+          "created_at": {"$first": "$created_at"},
+          "updated_at": {"$first": "$updated_at"},
+          "description": {"$first": "$description"},
+          "time_allocations": {"$push": "$time_allocations"},
+        }
+      },
+      {"$project": {"task_start_at": 0}},
+    ]
+
+  else:
+    query = {}
+
+    sort_field = (
+      "used_due_date"
+      if filters.sort_by == "due_date"
+      else "used_start_date"
+      if filters.sort_by == "start_date"
+      else filters.sort_by
+    )
+
+    sort_order = 1 if filters.sort_order == "asc" else -1
+
+    if filters.status is not None:
+      query["status"] = filters.status
+    if filters.priority is not None:
+      query["priority"] = filters.priority
+    if filters.tags:
+      query["$or"] = [
+        {"tags": {"$elemMatch": {"name": {"$regex": tag, "$options": "i"}}}}
+        for tag in filters.tags.split(",")
+      ]
+    if filters.from_date and filters.to_date and filters.range_type:
+      from_date = datetime.combine(filters.from_date, time(0, 0))
+      to_date = datetime.combine(filters.to_date, time(23, 59, 59))
+      if filters.range_type == "start_date":
+        query["used_start_date"] = {"$gte": from_date, "$lte": to_date}
+      else:
+        query["used_due_date"] = {"$gte": from_date, "$lte": to_date}
+
+    pipeline = [
+      {
+        "$addFields": {
+          "used_start_date": {
+            "$cond": {
+              "if": "$smart_scheduling",
+              "then": "$start_date",
+              "else": {
+                "$getField": {
+                  "field": "start_at",
+                  "input": {"$arrayElemAt": ["$time_allocations", 0]},
+                }
+              },
+            }
+          },
+          "used_due_date": {
+            "$cond": {
+              "if": "$smart_scheduling",
+              "then": "$due_date",
+              "else": {
+                "$getField": {
+                  "field": "end_at",
+                  "input": {"$arrayElemAt": ["$time_allocations", 0]},
+                }
+              },
+            }
+          },
+        }
+      },
+      {"$match": query},
+      {"$sort": {sort_field: sort_order}},
+    ]
+
+  # Pagination
+  skip = (filters.page - 1) * filters.limit
+  pipeline.extend([{"$skip": skip}, {"$limit": filters.limit}])
+
+  results = await Task.aggregate(pipeline).to_list()
+
+  if results:
+    tasks = []
+    for task in results:
+      task["id"] = task.pop("_id")
+      tasks.append(Task(**task))
+    return tasks
+  return None
 
 
 async def create_task(task: TaskCreate):
-	from app.services.taskService.autoScheduler import find_optimal_time
+  from app.services.taskService.autoScheduler import find_optimal_time
 
-	task_dict = task.model_dump()
-	if task.smart_scheduling:
-		time_allocations = await find_optimal_time(task)
-		if time_allocations:
-			task_dict["time_allocations"] = time_allocations
+  task_dict = task.model_dump()
+  if task.smart_scheduling:
+    time_allocations = await find_optimal_time(task)
+    if time_allocations:
+      task_dict["time_allocations"] = time_allocations
 
-	new_task = Task(**task_dict)
-	await new_task.insert()
-	if new_task.id:
-		return new_task
-	return None
+  new_task = Task(**task_dict)
+  await new_task.insert()
+  if new_task.id:
+    return new_task
+  return None
 
 
 async def update_task(id: str, updated_data: TaskUpdate):
-	from app.services.taskService.autoScheduler import find_optimal_time
-	from app.services.taskService.rescheduler import reschedule_overdue_tasks
+  from app.services.taskService.autoScheduler import find_optimal_time
+  from app.services.taskService.rescheduler import reschedule_overdue_tasks
 
-	# Get existing task
-	existing_task = await get_task_by_id(id)
-	if not existing_task:
-		return None, None
+  # Get existing task
+  existing_task = await get_task_by_id(id)
+  if not existing_task:
+    return None, None
 
-	# Check if updated fields related to the smart scheduling feature
-	existing_data_dict = existing_task.model_dump()
-	updated_data_dict = updated_data.model_dump()
+  # Check if updated fields related to the smart scheduling feature
+  existing_data_dict = existing_task.model_dump()
+  updated_data_dict = updated_data.model_dump()
 
-	# Recalculate optimal time if these fields change
-	fields = ["duration", "start_date", "due_date", "split", "scheduling_hour_id"]
-	updated_fields = []
-	if existing_data_dict["smart_scheduling"] and updated_data_dict["smart_scheduling"]:
-		# Select changed fields
-		for field in fields:
-			old = existing_data_dict[field]
-			new = updated_data_dict[field]
-			if not old and not new:
-				continue
-			if old != new or (not old and new) or (old and not new):
-				updated_fields.append(field)
+  # Recalculate optimal time if these fields change
+  fields = ["duration", "start_date", "due_date", "split", "scheduling_hour_id"]
+  updated_fields = []
+  if existing_data_dict["smart_scheduling"] and updated_data_dict["smart_scheduling"]:
+    # Select changed fields
+    for field in fields:
+      old = existing_data_dict[field]
+      new = updated_data_dict[field]
+      if not old and not new:
+        continue
+      if old != new or (not old and new) or (old and not new):
+        updated_fields.append(field)
 
-	# Recalculate optimal time
-	if (updated_fields and updated_data_dict["smart_scheduling"]) or (
-		not existing_data_dict["smart_scheduling"] and updated_data_dict["smart_scheduling"]
-	):
-		time_allocations = await find_optimal_time(updated_data, PydanticObjectId(id))
-		if time_allocations:
-			updated_data_dict["time_allocations"] = time_allocations
+  # Recalculate optimal time
+  if (updated_fields and updated_data_dict["smart_scheduling"]) or (
+    not existing_data_dict["smart_scheduling"] and updated_data_dict["smart_scheduling"]
+  ):
+    time_allocations = await find_optimal_time(updated_data, PydanticObjectId(id))
+    if time_allocations:
+      updated_data_dict["time_allocations"] = time_allocations
 
-	# Update task in MongoDB
-	updated_data_dict["id"] = id  # Add id to updated data
-	updated_task = Task(**updated_data_dict)
-	await updated_task.save()
+  # Update task in MongoDB
+  updated_data_dict["id"] = id  # Add id to updated data
+  updated_task = Task(**updated_data_dict)
+  await updated_task.save()
 
-	# If there are overdue tasks, reschedule them
-	if updated_task:
-		# Get overdue tasks rescheduled successfully
-		updated_overdue_tasks = await reschedule_overdue_tasks(updated_task.id)
+  # If there are overdue tasks, reschedule them
+  if updated_task:
+    # Get overdue tasks rescheduled successfully
+    updated_overdue_tasks = await reschedule_overdue_tasks(updated_task.id)
 
-		if updated_overdue_tasks:  # Update data in Mongodb
-			for updated_overdue_task in updated_overdue_tasks:
-				await updated_overdue_task.save()
+    if updated_overdue_tasks:  # Update data in Mongodb
+      for updated_overdue_task in updated_overdue_tasks:
+        await updated_overdue_task.save()
 
-		return updated_task, updated_overdue_tasks
-	return None, None
+    return updated_task, updated_overdue_tasks
+  return None, None
 
 
 async def delete_task(id: str):
-	from app.services.taskService.rescheduler import reschedule_overdue_tasks
+  from app.services.taskService.rescheduler import reschedule_overdue_tasks
 
-	deleted_task = await Task.get(id)
-	await deleted_task.delete()
+  deleted_task = await Task.get(id)
+  await deleted_task.delete()
 
-	if deleted_task:
-		# Get overdue tasks rescheduled successfully
-		updated_overdue_tasks = await reschedule_overdue_tasks()
+  if deleted_task:
+    # Get overdue tasks rescheduled successfully
+    updated_overdue_tasks = await reschedule_overdue_tasks()
 
-		if updated_overdue_tasks:  # Update data in Mongodb
-			for updated_overdue_task in updated_overdue_tasks:
-				await updated_overdue_task.save()
+    if updated_overdue_tasks:  # Update data in Mongodb
+      for updated_overdue_task in updated_overdue_tasks:
+        await updated_overdue_task.save()
 
-		return deleted_task, updated_overdue_tasks
-	return None, None
+    return deleted_task, updated_overdue_tasks
+  return None, None
